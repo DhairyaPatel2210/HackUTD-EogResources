47943e5524e1f37a48fff9076ca11e91c38e5f96
DhairyaPatel2210 <pateldhairya283@gmail.com>
2024-11-17 16:21:58 +0530

    feat(frontend): integrated token-validation api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import { AppRouter } from '@/components/router/AppRouter';
+import { Spinner } from '@/components/ui/Spinner';
 
 import './index.css';
+import { useApp } from './hooks/useApp';
 
 function App() {
+
+  const { loading } = useApp();
+
+  if (loading) {
+    return <div className='flex flex-row justify-center items-center w-screen h-screen'>
+      <Spinner />
+    </div>;
+  }
+
   return (
     <AppRouter />
   );
